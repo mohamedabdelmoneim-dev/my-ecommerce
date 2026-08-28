e4bba759f572b5ed812a590322df686ff4cd9671
Mohamed Abdelmoneim <men3m1232006@gmail.com>
2026-08-28 03:46:23 +0300

    Add vercel config and dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-ecommerce/",
+  base: process.env.VERCEL ? "/" : "/my-ecommerce/",
   plugins: [react()],
   optimizeDeps: {
     include: ['react-fast-marquee']
